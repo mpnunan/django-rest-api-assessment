fdbf9d74d496b6132e91e7960c2b80173b289cf4
Michael Nunan <michael.nunan@outlook.com>
2023-12-07 21:24:41 -0600

    crud for artists and genres

--- tunaapi/views/artist.py
+++ tunaapi/views/artist.py
@@ -19,7 +19,28 @@ class ArtistView(ViewSet):
         artists = Artist.objects.all()
         serializer = ArtistSerializer(artists, many=True)
         return Response(serializer.data)
-          
+    
+    def create(self, request):
+        artist = Artist.objects.create(
+            name=request.data["name"],
+            age=request.data["age"],
+            bio=request.data["bio"]
+        )
+        serializer = ArtistSerializer(artist)
+        return Response(serializer.data)
+    
+    def update(self, request, pk):
+        artist = Artist.objects.get(pk=pk)
+        artist.name=request.data["name"]
+        artist.age=request.data["age"]
+        artist.bio=request.data["bio"]
+        artist.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        artist = Artist.objects.get(pk=pk)
+        artist.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
--- tunaapi/views/genre.py
+++ tunaapi/views/genre.py
@@ -19,7 +19,24 @@ class GenreView(ViewSet):
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data)
-
+    
+    def create(self, request):
+        genre = Genre.objects.create(
+            description=request.data["description"]
+        )
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data)
+    
+    def update(self, request, pk):
+        genre = Genre.objects.get(pk=pk)
+        genre.description=request.data["description"]
+        genre.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        genre = Genre.objects.get(pk=pk)
+        genre.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
